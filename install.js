725cad5ab8e85040bcc01628a6c1770c94320d9f
AtomJump <peter@atomjump.com>
2017-09-01 17:45:29 +1200

    missing a lib

--- install.js
+++ install.js
@@ -24,6 +24,7 @@ var queryString = require('querystring');
 var fs = require('fs');
 var fsExtra = require('fs-extra');
 const cheerio = require('cheerio');
+var exec = require('child_process').exec;
 
 var verbose = false;
 
